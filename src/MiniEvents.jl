d9c95bf23d6cf85b0969308d19701021f7639a55
Martin Hinsch <hinsch.martin@gmail.com>
2022-12-09 15:51:51 +0000

    first working full macro set

--- src/MiniEvents.jl
+++ src/MiniEvents.jl
@@ -1,3 +1,5 @@
+using MacroTools
+
 
 include("Events.jl")
 using .Events
@@ -44,3 +46,128 @@ function refresh!(agents, alist)
 	end
 end
 
+
+function gen_event_count_fn(decl, n)
+    ag_type = decl.args[2]
+    quote
+        function $(esc(:event_count))(::Type{$ag_type}) 
+            $n
+        end
+    end
+end
+
+
+function gen_refresh_fn(decl)
+    ag_type = decl.args[2]
+    quote
+        function $(esc(:refresh!))(agent::$ag_type, alist)
+            change_rates!(agent, alist, calc_rates(agent))
+        end
+    end
+end
+
+
+function gen_calc_rate_fn(decl, conds, rates)
+    n = length(conds)
+    VT = :(SVector{$n, Float64})
+
+    con_call = :($VT())
+    for (c, r) in zip(conds, rates)
+        arg = :($(esc(c)) ? $(esc(r)) : 0.0)
+        push!(con_call.args, arg)
+    end
+
+    quote
+        function $(esc(:calc_rates))($(esc(decl))) 
+            $con_call
+        end
+    end
+end
+
+function gen_step_fn(decl, actions)
+    check_actions = quote end
+
+    for (i, a) in enumerate(actions)
+        check = :(if (r -= rates[$i]) < 0
+                      $(esc(a))
+                      return
+                  end)
+        push!(check_actions.args, check)
+    end
+
+    l_type = :(SVector{$(length(actions)), Float64})
+    ag_name = decl.args[1]
+    ag_type = decl.args[2]
+
+    quote
+        function $(esc(:step))(alist :: $(esc(:EventList)){$ag_type, $l_type}, rnum)
+            i, r = lookup(alist.sums, rnum)
+
+            ag_actions = alist.events[i]
+            rates = ag_actions.rates
+            $(esc(ag_name)) = ag_actions.agent
+
+            $(esc(:refresh!))(p) = refresh!(p, alist)
+
+            $check_actions
+
+            error("somthing went wrong!")
+        end
+    end
+end
+
+
+macro processes(decl, conds, rates, actions)
+    res = quote end
+
+    fd = gen_event_count_fn(decl, length(rmlines(conds).args))
+    push!(res.args, fd)
+
+    fd = gen_refresh_fn(decl)
+    push!(res.args, fd)
+
+    fd = gen_calc_rate_fn(decl, rmlines(conds).args, rmlines(rates).args)
+    push!(res.args, fd)
+
+    fd = gen_step_fn(decl, rmlines(actions).args)
+    push!(res.args, fd)
+
+    res
+end
+
+
+macro simulation(name, types...)
+    decl = :(struct $name end)
+    members = decl.args[3].args
+
+    constr = :($(esc(name))())
+    args = constr.args
+
+    step_call = :($(esc(:dispatch_step))())
+    step_args = step_call.args
+
+    aa_block = quote end
+    aa_args = aa_block.args
+
+    for (i, t) in enumerate(types)
+        m_name = Symbol("m_$(i)")
+        m_type = :(EventList{$t, SVector{event_count($t), Float64}})
+        m_e = :($m_name :: $m_type)
+        m_aa = :(Events.add_agent!(a :: $t, s :: $name) = 
+                 Events.add_agent!(a, s.$m_name, calc_rates(a)))
+
+        push!(members, m_e)
+        push!(args, :($m_type()))
+        push!(step_args, :(sim.$m_name))
+        push!(aa_args, m_aa)
+    end
+
+    quote
+        $decl
+        $(esc(name))() = $constr
+
+        $(esc(:step))(sim :: $name) = $step_call
+
+        $aa_block
+    end
+end
