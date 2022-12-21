e1445c8f26d4806418b438884b5a98535862a3ef
Martin Hinsch <hinsch.martin@gmail.com>
2022-12-21 14:38:56 +0000

    tests

--- test/test_sched.jl
+++ test/test_sched.jl
@@ -1,11 +1,12 @@
 push!(LOAD_PATH, "./src")
 
-using SimpleAgentEvents
+using MiniEvents
 
+@enum AState asleep awake
 
 mutable struct A1
 	id :: Int
-	state :: Int
+	state :: AState
 	food :: Int
 end
 
@@ -15,84 +16,76 @@ end
 
 Model() = Model([])
 
-struct Simulation
-	model :: Model
-end
-
 
 function A1(i)
-	A1(i, 0, 0)
+	A1(i, asleep, 0)
 end
 
 
-function wakeup(a::A1, model)
-#	println("$(a.id): wake up")
-	a.state = 1
-
-	[a]
+function wakeup(a::A1)
+	println("$(a.id): wake up")
+	a.state = awake
 end
 
-function sleep(a::A1, model)
-#	println("$(a.id): sleep")
+function sleep(a::A1)
+	println("$(a.id): sleep")
 	a.food -= 1
-
-	[a]
 end
 
-function walk(a::A1, model)
-#	println("$(a.id): walk")
+function walk(a::A1)
+	println("$(a.id): walk")
 	a.food -= 1
-
-	[a]
 end
 
-function forage(a::A1, model)
-#	println("$(a.id): forage")
+function forage(a::A1)
+	println("$(a.id): forage")
 	a.food += rand(1:5)
-
-	[a]
 end
 
-function fallasleep(a::A1, model)
-#	println("$(a.id): go to bed")
-	a.state = 0
-
-	[a]
+function fallasleep(a::A1)
+	println("$(a.id): go to bed")
+	a.state = asleep
 end
 
-
-const simulation = Simulation(Model())
-
-
-@processes SimpleTest simulation self::A1 begin
+@events self::A1 begin
 	# wake up
-	@poisson(2.0)	~ self.state == 0					=> wakeup(self, simulation.model)
-	@poisson(1.0)	~ self.state == 0					=> sleep(self, simulation.model)
-	@poisson(0.5)	~ self.state == 1 && self.food > 3	=> walk(self, simulation.model)
-	@poisson(1.0) 	~ self.state == 1 && self.food <= 3	=> forage(self, simulation.model)
-	@poisson(3.0)	~ self.state == 1 && self.food > 1	=> fallasleep(self, simulation.model)
+	@rate(2.0)	~ self.state == asleep				=> begin
+		wakeup(self); @r self end
+	@rate(1.0)	~ self.state == asleep				=> begin
+		sleep(self); @r self end
+	@rate(0.5)	~ self.state == awake && self.food > 3	=> begin
+		walk(self); @r self end
+	@rate(1.0) 	~ self.state == awake && self.food <= 3	=> begin
+		forage(self); @r self end
+	@rate(3.0)	~ self.state == awake && self.food > 1	=> begin
+		fallasleep(self); @r self end
 end
 
-@add_processes SimpleTest simulation m::Model begin
-	@poisson(10.0) ~ length(m.pop) < 10 => begin
-		push!(m.pop, A1(1))
-		SimpleTest.spawn(m.pop[end], simulation)
-		println("added agent")
-		[m]
+@events m::Model begin
+	@repeat(1.0, 0.1) => begin
+		if length(m.pop) < 10
+			push!(m.pop, A1(length(m.pop)))
+			spawn!(m.pop[end], @sim())
+			println("added agent")
+		end
 	end
 end
 
-function setup()
-	SimpleTest.spawn(simulation.model, simulation)
+@simulation Sim A1 Model
+
+function setup(sim)
+	spawn!(Model(), sim)
 end
 
-function run(n)
+function run(n, sim)
 	for i in 1:n
-		SimpleTest.next!()
+		next_event!(sim)
 	end
-	println(SimpleTest.time_next())
+	println(now(sim))
 end
 
-setup()
+const simulation = Sim()
 
-run(10)
+setup(simulation)
+
+run(10, simulation)
